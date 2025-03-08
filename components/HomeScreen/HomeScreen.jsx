b0a03a4903d2a2b4e56d8652b6c0de2eb40925ee
Igor de Souza <igordesouzadezembro19@gmail.com>
2025-03-08 01:36:16 -0300

    adicionando principais texto e scroll lateral

--- components/HomeScreen/HomeScreen.jsx
+++ components/HomeScreen/HomeScreen.jsx
@@ -11,18 +11,27 @@ import {
   SafeAreaView,
   TouchableOpacity,
   TextInput,
+  ScrollView,
 } from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+
 export default function HomeScreen() {
+
+  const handleLocation = (loc)=>{
+    console.log('location' , loc)
+  }
   const [showSearch, toggleSearch] = useState(false);
   const [locations, setLocations] = useState([1,2,3]);
+
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
       <Image
-        blurRadius={65}
-        source={require("../../assets/images/bg.png")}
+        resizeMode="cover"
+        blurRadius={1}
+        source={require("../../assets/images/teste4.png")}
         style={styles.backgroundImage}
       />
       <SafeAreaView style={tw`flex flex-1`}>
@@ -60,13 +69,16 @@ export default function HomeScreen() {
             locations.length>0 &&  showSearch? (
                 <View style={tw`absolute w-full bg-gray-400 top-16 rounded-3xl`}>{
                     locations.map((loc, index)=>{
+                        // let showBorder = index !== locations.length
+                        // let borderClass = showBorder? 'border-b-2 border-b-gray-300' : ''
                         return(
                             <TouchableOpacity
+                            onPress={() => handleLocation(loc)}
                             key={index}
-                            style={tw`flex-row items-center border-0 p-3 px-4 mb-1 border-bg-gray-400`}>
-
+                            style={tw`flex-row items-center border-0 p-3 px-4 mb-1`}>
+                                <Ionicons name="location-outline" size={20} color="black" style={tw`mr-2`}/>
                                 <Text style={tw`text-black text-base font-semibold`}>
-                                    London, united kingdom
+                                    Rio do Sul, Santa Catarina
                                 </Text>
                             </TouchableOpacity>
                         )   
@@ -74,6 +86,58 @@ export default function HomeScreen() {
                 } </View>
             ):null
           }
+        </View>
+
+        <View style={tw`mx-4 flex justify-around  flex-1  mb-2`}>
+          <Text style={tw`text-white text-center text-3xl font-bold`}>Rio do Sul,
+          <Text style={tw`text-lg text-white font-semibold text-2xl`}>  SC</Text>
+          </Text>
+          <View style={tw`flex-row justify-center `}>
+          <Image source={require("../../assets/images/partlycloudy.png")} style={tw`w-48 h-48`} />
+          </View>
+          <View style={tw`space-y-3`}>
+            <Text style={tw` text-center text-white text-7xl font-bold`}>
+            28&#176;
+            </Text>
+            <Text style={tw` text-center text-white text-xl tracking-widest `}>
+            Partlycloud
+            </Text>
+          </View>
+          <View style={tw`flex-row justify-between mx-4`}>
+            <View style={tw`space-x-2 flex-row items-center`}>
+                <Image source={require("../../assets/icons/wind.png")} style={tw`w-6 h-6`} />  
+                <Text style={tw`text-white text-base font-semibold ml-2`}>
+                    22km
+                </Text>
+            </View>
+            <View style={tw`space-x-2 flex-row items-center`}>
+                <Image source={require("../../assets/icons/drop.png")} style={tw`w-6 h-6`} />  
+                <Text style={tw`text-white text-base font-semibold ml-2`}>
+                    23%
+                </Text>
+            </View>
+            <View style={tw`space-x-2 flex-row items-center`}>
+                <Image source={require("../../assets/icons/sun.png")} style={tw`w-6 h-6`} />  
+                <Text style={tw`text-white text-base font-semibold ml-2`}>
+                    6:05 AM
+                </Text>
+            </View>
+          </View>
+        </View>
+
+        {/* para os próximos dias  */}
+        <View style={tw`space-y-3 mb-2`}>
+          <View style={tw`flex-row items-center mx-4 space-x-2`}>
+          <Ionicons name="calendar-outline" size={22} color="white" style={tw`mr-2`}/>
+          <Text style={tw`text-white text-base`}>
+            Previsão para o dia
+          </Text>
+          </View>
+          <ScrollView horizontal contentContainerStyle={{paddingHorizontal: 15}} showsHorizontalScrollIndicator={false} >
+            <View style={tw`flex justify-center items-center w-26 rounded-3xl py-3 space-y-1 mr-4 `}>
+            <Image source={require("../../assets/images/heavyrain.png")} style={tw`w-16 h-16`} />
+            </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     </View>
